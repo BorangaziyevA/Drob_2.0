0c181005f9f2b544d7605bd3392a48848b61eb1f
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-04-15 21:40:04 +0600

    Update Drob.h

--- Drobi2/Drob.h
+++ Drobi2/Drob.h
@@ -13,15 +13,20 @@ public:
 	Drob operator-=(const Drob &b);
 	Drob operator*=(const Drob &b);
 	Drob operator/=(const Drob &b);
+	Drob& operator++();
+	Drob operator++(int);
+	Drob& operator--();
+	Drob operator--(int);
 	void setX(int x);
 	void setY(int y);
 	int getX() const;
 	int getY() const;
-	void print();
+	void print()const;
 	double getFraction() const;
 };
 
-
+ostream& operator<<(ostream& os, const Drob& obj);
+istream& operator>>(istream& is, Drob& obj);
 Drob operator+(const Drob &a, const Drob &b);
 bool operator==(const Drob &a, const Drob &b);
 bool operator>=(const Drob &a, const Drob &b);
